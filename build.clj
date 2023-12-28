ed75d75396b28a2e836d9eb19d705027782c7654
Emlyn Corrin <emlyn.corrin@gmail.com>
2023-12-28 11:17:52 +0100

    Version 0.1.0

--- build.clj
+++ build.clj
@@ -5,9 +5,7 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'com.github.emlyn/grid)
-(def version "0.1.0-SNAPSHOT")
-#_ ; alternatively, use MAJOR.MINOR.COMMITS:
-(def version (format "1.0.%s" (b/git-count-revs nil)))
+(def version "0.1.0")
 (def class-dir "target/classes")
 
 (defn test "Run all the tests." [opts]
