7e4c336a53725fc9fe487cc7b9c96a67789bbc31
Emlyn Corrin <emlyn.corrin@gmail.com>
2023-12-28 22:49:33 +0100

    Improve construction, support construction from map or map of maps, improve tests

--- src/emlyn/grid/type.clj
+++ src/emlyn/grid/type.clj
@@ -4,32 +4,103 @@
             [potemkin :refer [def-map-type]]))
 
 (defn- pos->index
+  "Get the index of a position in a grid."
   [[w h] [x y]]
   (when (and (< -1 x w)
              (< -1 y h))
     (+ x (* w y))))
 
 (defn- shape->keys
+  "Get the keys of a grid."
   [[w h]]
   (for [y (range h)
         x (range w)]
     [x y]))
 
+(defn- map-data?
+  "Is the init data a valid map of coordinate to value?"
+  [data]
+  (and (map? data)
+       (every? #(and (sequential? %)
+                     (= 2 (count %))
+                     (every? nat-int? %)) (keys data))))
+
+(defn mapmap-data?
+  "Is the init data a valid map of x to map of y to value?"
+  [data]
+  (and (map? data)
+       (every? nat-int? (keys data))
+       (every? #(and (map? %)
+                     (every? nat-int? (keys %))) (vals data))))
+
+(defn- infer-shape
+  "Infer the shape of a grid from its init data."
+  [data]
+  (cond
+    (and (sequential? data)
+         (every? #(or (sequential? %) (string? %)) data)
+         (apply = (map count data)))
+    [(count (first data))
+     (count data)]
+
+    (map-data? data)
+    (->> (keys data)
+         (reduce (partial map max) [-1 -1])
+         (mapv inc))
+
+    (mapmap-data? data)
+    [(inc (apply max -1 (keys data)))
+     (inc (apply max -1 (mapcat keys (vals data))))]))
+
 (defn- massage
+  "Massage init data into a vector of cells."
   [w h data]
-  (if (fn? data)
+  (cond
+    (nil? data)
+    (into [] (repeat (* w h) nil))
+
+    (fn? data)
     (mapv (partial apply data) (shape->keys [w h]))
+
+    (and (or (sequential? data) (string? data))
+         (= (* w h) (count data)))
+    (vec data)
+
+    (map-data? data)
+    (reduce-kv (fn [cells pos val]
+                 (if-let [i (pos->index [w h] pos)]
+                   (assoc cells i val)
+                   cells))
+               (into [] (repeat (* w h) nil))
+               data)
+
+    (mapmap-data? data)
+    (reduce-kv (fn [cells x ymap]
+                 (reduce-kv (fn [cells y val]
+                              (if-let [i (pos->index [w h] [x y])]
+                                (assoc cells i val)
+                                cells))
+                            cells
+                            ymap))
+               (into [] (repeat (* w h) nil))
+               data)
+
+    :else
     (let [data (if (string? data)
                  (str/split-lines data)
                  data)]
-      (reduce into
-              []
-              (take h
-                    (concat (map #(take w (concat % (repeat nil))) data)
-                            (repeat (repeat w nil))))))))
+      (if (and (sequential? data)
+               (every? #(or (sequential? %) (string? %)) data))
+        (reduce into
+                []
+                (take h
+                      (concat (map #(take w (concat % (repeat nil))) data)
+                              (repeat (repeat w nil)))))
+        (throw (IllegalArgumentException. "Invalid init data."))))))
 
 (def-map-type Grid [shape cells]
-  (get [_ pos default] (get cells (pos->index shape pos) default))
+  (get [_ pos default] (when-let [i (pos->index shape pos)]
+                         (get cells i default)))
   (assoc [_ pos val] (if-let [i (pos->index shape pos)]
                        (Grid. shape (assoc cells i val))
                        (throw (IndexOutOfBoundsException.))))
@@ -42,34 +113,28 @@
   (count [_] (apply * shape)))
 
 (defn width
+  "The width of a grid in cells."
   [grid]
   (first (.shape grid)))
 
 (defn height
+  "The height of a grid in cells."
   [grid]
   (second (.shape grid)))
 
 (defn as-rows
+  "Get the grid as a sequence of rows."
   [grid]
   (partition (width grid) (.cells grid)))
 
 (defn grid
-  ([init]
-   (cond
-     (string? init)
-     (grid (str/split-lines init))
-
-     (and (sequential? init)
-          (every? #(or (sequential? %) (string? %)) init))
-     (if (apply = (map count init))
-       (->Grid [(count (first init))
-                (count init)]
-               (reduce into [] init))
+  "Construct a grid."
+  ([data]
+   (if (string? data)
+     (grid (str/split-lines data))
+     (if-let [[w h] (infer-shape data)]
+       (grid w h data)
        (throw (IllegalArgumentException.
-               "All rows must have the same length.")))
-
-     :else
-     (throw (IllegalArgumentException.
-             "Init value for grid must be a sequence of sequences or a string."))))
+               "Unable to infer shape from init data.")))))
   ([w h & [init]]
    (->Grid [w h] (massage w h init))))
--- test/emlyn/grid_test.clj
+++ test/emlyn/grid_test.clj
@@ -1,39 +1,105 @@
 (ns emlyn.grid-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest testing is]]
             [emlyn.grid :as g]))
 
-(deftest init-empty-test
-  (let [g (g/grid 2 3)]
-    (is (= 2 (g/width g)))
-    (is (= 3 (g/height g)))
-    (is (nil? (g [0 0])))
-    (let [gg (-> g
-                 (assoc [0 0] 1)
-                 (assoc [1 0] 2)
-                 (assoc [0 1] 3)
-                 (assoc [1 1] 4)
-                 (assoc [0 2] 5)
-                 (assoc [1 2] 6))]
-      (is (= 1 (gg [0 0])))
-      (is (= 6 (gg [1 2])))
-      (is (nil? (gg [2 0])))
-      (is (= [1 2 3 4 5 6] (vals gg))))))
+(deftest construct-with-shape-test
+  (testing "Construct from just shape args"
+    (let [g (g/grid 2 3)]
+      (is (= 2 (g/width g)))
+      (is (= 3 (g/height g)))
+      (is (nil? (g [0 0])))
+      (is (= (repeat 6 nil) (vals g)))))
+  (testing "Construct from shape and flat vector"
+    (let [g (g/grid 2 3 [1 2 3 4 5 6])]
+      (is (= 2 (g/width g)))
+      (is (= 3 (g/height g)))
+      (is (= 1 (g [0 0])))
+      (is (= (range 1 7) (vals g)))))
+  (testing "Construct from shape and function"
+    (let [g (g/grid 4 4 *)]
+      (is (= [0 0 0 0
+              0 1 2 3
+              0 2 4 6
+              0 3 6 9] (vals g)))))
+  (testing "Construct from shape and vectors"
+    (let [g (g/grid 2 3 [[1 2] [3 4] [5 6]])]
+      (is (= 2 (g/width g)))
+      (is (= 3 (g/height g)))
+      (is (= 1 (g [0 0])))
+      (is (= (range 1 7) (vals g)))))
+  (testing "Construct from shape and map"
+    (let [g (g/grid 3 2 {[0 0] 1
+                         [1 1] 2
+                         [2 0] 3
+                         [2 1] 4})]
+      (is (= 3 (g/width g)))
+      (is (= 2 (g/height g)))
+      (is (= 1 (g [0 0])))
+      (is (= [ 1 nil 3
+              nil 2  4] (vals g)))))
+  (testing "Construct from shape and map of maps"
+    (let [g (g/grid 3 2 {0 {0 1}
+                         1 {1 2}
+                         2 {0 3
+                            1 4}})]
+      (is (= 3 (g/width g)))
+      (is (= 2 (g/height g)))
+      (is (= 1 (g [0 0])))
+      (is (= [1 nil 3
+              nil 2 4] (vals g))))))
 
-(deftest init-string-test
-  (let [g (g/grid "abc\ndef\nghi\njkl")]
-    (is (= 3 (g/width g)))
-    (is (= 4 (g/height g)))
-    (is (= \a (g [0 0])))
-    (is (= \l (g [2 3])))
-    (is (nil? (g [3 0])))))
+(deftest construct-without-shape-test
+  (testing "Construct from string"
+    (let [g (g/grid "abc\ndef\nghi\njkl")]
+      (is (= 3 (g/width g)))
+      (is (= 4 (g/height g)))
+      (is (= \a (g [0 0])))
+      (is (= \l (g [2 3])))
+      (is (nil? (g [3 0])))))
+  (testing "Construct from vectors"
+    (let [g (g/grid [[1 2] [3 4] [5 6]])]
+      (is (= 2 (g/width g)))
+      (is (= 3 (g/height g)))
+      (is (= 1 (g [0 0])))
+      (is (= (range 1 7) (vals g)))))
+  (testing "Construct from map"
+    (let [g (g/grid {[0 0] 1
+                     [1 1] 2
+                     [2 0] 3
+                     [2 1] 4})]
+      (is (= 3 (g/width g)))
+      (is (= 2 (g/height g)))
+      (is (= 1 (g [0 0])))
+      (is (= [1  nil 3
+              nil 2  4] (vals g)))))
+  (testing "Construct from map of maps"
+    (let [g (g/grid 3 2 {0 {0 1}
+                         1 {1 2}
+                         2 {0 3
+                            1 4}})]
+      (is (= 3 (g/width g)))
+      (is (= 2 (g/height g)))
+      (is (= 1 (g [0 0])))
+      (is (= [1  nil 3
+              nil 2  4] (vals g))))))
 
-(deftest init-fn-test
-  (let [g (g/grid 3 2 +)]
-    (is (= [0 1 2 1 2 3] (vals g)))))
-
-(deftest bad-init-test
+(deftest construct-error-test
   (is (thrown? IllegalArgumentException (g/grid "123\n45")))
-  (is (thrown? IllegalArgumentException (g/grid [1 2 3 4]))))
+  (is (thrown? IllegalArgumentException (g/grid [1 2 3 4])))
+  (is (thrown? IllegalArgumentException (g/grid 2 3 [1 2 3 4])))
+  (is (thrown? IllegalArgumentException (g/grid {[1 -1] 3})))
+  (is (thrown? IllegalArgumentException (g/grid {1 {-1 3}}))))
+
+(deftest assoc-test
+  (let [g (g/grid 3 2 [[1 2 3] [4 5 6]])]
+    (is (= [1 2 3 4 5 6] (vals g)))
+    (let [g (assoc g [1 1] 9)]
+      (is (= 9 (g [1 1])))
+      (is (= [1 2 3 4 9 6] (vals g)))
+      (let [g (dissoc g [0 1])]
+        (is (nil? (g [0 1])))
+        (is (= [1 2 3
+                nil 9 6] (vals g)))))))
 
 (deftest map-vals-test
   (let [g1 (g/grid [[1 3 5] [2 4 6]])
