e3ff84b7ea91fda4ba1a473d3c5cce687dd4788c
Emlyn Corrin <emlyn.corrin@gmail.com>
2023-12-30 23:03:56 +0100

    Also pprint as vecs

--- src/emlyn/grid/print.clj
+++ src/emlyn/grid/print.clj
@@ -1,6 +1,7 @@
 (ns emlyn.grid.print
   (:require [emlyn.grid.type]
-            [emlyn.grid.convert :refer [to-vecs]])
+            [emlyn.grid.convert :refer [to-vecs]]
+            [clojure.pprint])
   (:import [emlyn.grid.type Grid]))
 
 (defmethod print-method Grid [grid writer]
@@ -9,3 +10,9 @@
 (defmethod print-dup Grid [grid writer]
   (.write writer "#emlyn/grid ")
   (print-method (to-vecs grid) writer))
+
+(defn pprint-grid
+  [grid]
+  (clojure.pprint/pprint (to-vecs grid)))
+
+(. clojure.pprint/simple-dispatch addMethod Grid pprint-grid)
