1712c067a78e1f9d1fc001ffd15e9041b31785f4
Emlyn Corrin <emlyn.corrin@gmail.com>
2024-01-07 20:16:02 +0000

    Bump version

--- build.clj
+++ build.clj
@@ -5,7 +5,7 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'com.github.emlyn/grid)
-(def version "0.2.0")
+(def version "0.2.1-SNAPSHOT")
 (def class-dir "target/classes")
 
 (defn test "Run all the tests." [opts]
